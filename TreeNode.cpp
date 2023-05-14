57dd6883950eeafa6ba086f80976b442a2191996
Laurens De Wachter <laurens.dewachter@hotmail.com>
2023-05-15 00:03:14 +0200

    Add is_brackets to TreeNode

--- TreeNode.cpp
+++ TreeNode.cpp
@@ -1,13 +1,15 @@
 #include "TreeNode.h"
 
 
-TreeNode::TreeNode(std::string value) : value(std::move(value)), parent(nullptr), children({}) {}
+TreeNode::TreeNode(std::string value, bool is_brackets) : value(std::move(value)), parent(nullptr), children({}),
+                                                          is_brackets(is_brackets) {}
 
 const std::string &TreeNode::getValue() const {
     return value;
 }
 
 void TreeNode::addChild(TreeNode *child) {
+    if (child == nullptr) return;
     child->parent = this;
     children.push_back(child);
 }
--- TreeNode.h
+++ TreeNode.h
@@ -6,7 +6,7 @@
 
 class TreeNode {
 public:
-    explicit TreeNode(std::string value);
+    explicit TreeNode(std::string value, bool is_brackets);
 
     [[nodiscard]] const std::string &getValue() const;
 
@@ -20,6 +20,7 @@ private:
     std::string value;
     TreeNode *parent;
     std::vector<TreeNode *> children;
+    bool is_brackets;
 };
 
 
